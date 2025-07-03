b97a5f5087d3c3e3736352c8899d6071a82b62b2
Sertug Serkan Guzel <66708115+sserkanguzel@users.noreply.github.com>
2025-07-03 11:53:59 +0300

    Update k8soperatortestdag.py

--- dags/k8soperatortestdag.py
+++ dags/k8soperatortestdag.py
@@ -2,21 +2,43 @@ from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 from datetime import datetime
 
+default_args = {
+    "start_date": datetime(2025, 1, 1),
+}
+
 with DAG(
-    dag_id='K8SPodOperator_Test',
-    start_date=datetime(2025, 1, 1),
+    dag_id="K8SPodOperator_Test",
+    default_args=default_args,
     schedule_interval=None,
-    description='Run a Kubernetes Pod from Airflow',
     catchup=False,
-    tags=['test'],
+    description="Run a Kubernetes Pod from Airflow using pod_template.yaml",
+    tags=["test"],
 ) as dag:
 
     run_pod = KubernetesPodOperator(
         task_id="run-echo",
         name="run-echo",
         namespace="airflow",
-        pod_template_file="/opt/airflow/pod_templates/pod_template.yaml",
+        config_file=None,  # Use in-cluster config
         is_delete_operator_pod=True,
-        config_file=None,
-        cmds=["echo", "Hello from KubernetesPodOperator!"]
+        pod_template_file="/opt/airflow/pod_templates/pod_template.yaml",
+        volumes=[
+            {
+                "name": "pod-template",
+                "configMap": {
+                    "name": "airflow-pod-template",
+                    "items": [
+                        {"key": "pod_template.yaml", "path": "pod_template.yaml"}
+                    ],
+                },
+            }
+        ],
+        volume_mounts=[
+            {
+                "name": "pod-template",
+                "mountPath": "/opt/airflow/pod_templates",
+                "readOnly": True,
+            }
+        ],
+        cmds=["echo", "Hello from KubernetesPodOperator!"],
     )
